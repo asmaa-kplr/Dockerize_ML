e36afe7971e5c186eda2ead1f428826fccc805fb
asmaa-kplr <123757632+asmaa-kplr@users.noreply.github.com>
2023-03-07 12:14:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,67 @@
-from flask import Flask, render_template
+import os
+
+
+import numpy as np
+from keras.models import load_model
+import keras.utils as image
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+import cv2
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+
+MODEL_PATH = './static/model/tounsiModel.h5'
+
+
+model = load_model(MODEL_PATH)
+
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(28, 28)) 
+    img_array = np.asarray(img)
+    x = cv2.cvtColor(img_array,cv2.COLOR_BGR2GRAY)
+    result = int(img_array[0][0][0])
+    print(result)
+    if result > 128:
+      img = cv2.bitwise_not(x)
+    else:
+      img = x
+    img = img/255
+    img = (np.expand_dims(img,0)) 
+
+    preds =  model.predict(img)
+    print(preds)
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+   
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        
+        f = request.files['file']
+
+        
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, './static/upload', secure_filename(f.filename))
+        f.save(file_path)
+
+        
+        preds = model_predict(file_path, model)
+        class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+        predicted_label = np.argmax(preds)
+        result = class_names[predicted_label]
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
